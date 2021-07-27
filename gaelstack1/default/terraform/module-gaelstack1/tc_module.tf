44feaad3bf8eeed0b4fa4e1548d82632390432ce
admin cycloid <disabled@email.cycloid>
2021-07-27 07:54:06 +0000

    Update Service Catalog Terraform: gaelstack1

---
+++ gaelstack1/default/terraform/module-gaelstack1/tc_module.tf
@@ -0,0 +1,7 @@
+module "gaelstack1" {
+  # aws_instance_AIAcj_instance_type = "t3.small"
+  # aws_instance_tQQWP_instance_type = "t3.small"
+  # aws_instance_uGiAX_instance_type = "t3.small"
+  source = "module-gaelstack1"
+}
+
---
+++ gaelstack1/default/terraform/module-gaelstack1/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_instance_AIAcj_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_tQQWP_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_uGiAX_instance_type" {
+  default = "t3.small"
+}
+
