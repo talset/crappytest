d1a748d60592050c1c6c75c9ade881a551f23c63
admin cycloid <disabled@email.cycloid>
2021-07-28 12:39:37 +0000

    Update Service Catalog Terraform: testestestset

---
+++ testestestset/default/terraform/module-testestestset/tc_module.tf
@@ -0,0 +1,6 @@
+module "testestestset" {
+  # aws_instance_NlYVE_instance_type = "t3.small"
+  # aws_instance_pzcgR_instance_type = "t3.small"
+  source = "module-testestestset"
+}
+
---
+++ testestestset/default/terraform/module-testestestset/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_instance_NlYVE_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_pzcgR_instance_type" {
+  default = "t3.small"
+}
+
