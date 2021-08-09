acb3e6a9641b96fe90a71748b90cfc9458b8c061
admin cycloid <disabled@email.cycloid>
2021-08-09 14:43:04 +0000

    Update Service Catalog Terraform: reuse_stack

--- reuse_stack/default/terraform/module-reuse_stack/variables.tf
+++ reuse_stack/default/terraform/module-reuse_stack/variables.tf
@@ -1,56 +1,56 @@
-variable "aws_instance_BmtNe_instance_type" {
+variable "aws_instance_CpsAY_instance_type" {
   default = "t3.small"
 }
 
-variable "aws_instance_Bqpmk_instance_type" {
+variable "aws_instance_EbvEP_instance_type" {
   default = "t2.medium"
 }
 
-variable "aws_instance_BuNZL_instance_type" {
+variable "aws_instance_FYpZg_instance_type" {
+  default = "t2.small"
+}
+
+variable "aws_instance_FmRNu_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_HEhkF_instance_type" {
   default = "t2.micro"
 }
 
-variable "aws_instance_Edanj_instance_type" {
+variable "aws_instance_THjjA_instance_type" {
   default = "c5.2xlarge"
 }
 
-variable "aws_instance_JSXaV_instance_type" {
-  default = "t2.small"
-}
-
-variable "aws_instance_LhWyi_instance_type" {
-  default = "t2.small"
-}
-
-variable "aws_instance_MPjYj_instance_type" {
-  default = "t3.medium"
-}
-
-variable "aws_instance_RMbWJ_instance_type" {
-  default = "t3.small"
-}
-
-variable "aws_instance_TMSOb_instance_type" {
-  default = "c5.2xlarge"
-}
-
-variable "aws_instance_UJEZY_instance_type" {
-  default = "t3.small"
-}
-
-variable "aws_instance_WeKMK_instance_type" {
+variable "aws_instance_XfVMp_instance_type" {
   default = "m3.medium"
 }
 
-variable "aws_instance_YnhJE_instance_type" {
+variable "aws_instance_dqLuu_instance_type" {
   default = "t3.small"
 }
 
-variable "aws_instance_jOjZB_instance_type" {
+variable "aws_instance_gellC_instance_type" {
   default = "c5.2xlarge"
 }
 
-variable "aws_instance_sKVpd_instance_type" {
+variable "aws_instance_lNdeI_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_ooNvG_instance_type" {
+  default = "c5.2xlarge"
+}
+
+variable "aws_instance_pogGA_instance_type" {
+  default = "t2.small"
+}
+
+variable "aws_instance_qudSS_instance_type" {
+  default = "t3.medium"
+}
+
+variable "aws_instance_xXeUR_instance_type" {
   default = "c5.2xlarge"
 }
 
