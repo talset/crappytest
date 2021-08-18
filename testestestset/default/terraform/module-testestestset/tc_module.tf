2c8b7906d04becbb5eb7f2a962db3110261bb453
talset <talset55@gmail.com>
2021-08-18 11:43:31 +0200

    clean

--- testestestset/default/terraform/module-testestestset/tc_module.tf
+++
@@ -1,6 +0,0 @@
-module "testestestset" {
-  # aws_instance_NlYVE_instance_type = "t3.small"
-  # aws_instance_pzcgR_instance_type = "t3.small"
-  source = "module-testestestset"
-}
-
--- testestestset/default/terraform/module-testestestset/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_instance_NlYVE_instance_type" {
-  default = "t3.small"
-}
-
-variable "aws_instance_pzcgR_instance_type" {
-  default = "t3.small"
-}
-
