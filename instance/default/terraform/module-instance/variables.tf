089d4284f588309d8733417bddff2c42e6b46239
Florian Lambert <talset55@gmail.com>
2021-08-18 09:29:01 +0000

    Update Service Catalog Terraform: instance

---
+++ instance/default/terraform/module-instance/variables.tf
@@ -0,0 +1,60 @@
+variable "aws_instance_AfouJ_instance_type" {
+  default = "c5.2xlarge"
+}
+
+variable "aws_instance_ByvQZ_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_Ieqkh_instance_type" {
+  default = "c5.2xlarge"
+}
+
+variable "aws_instance_SagoX_instance_type" {
+  default = "m3.medium"
+}
+
+variable "aws_instance_StiZW_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_WgXOj_instance_type" {
+  default = "t3.medium"
+}
+
+variable "aws_instance_bdfhn_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_cMnXd_instance_type" {
+  default = "t2.small"
+}
+
+variable "aws_instance_ejEtz_instance_type" {
+  default = "c5.2xlarge"
+}
+
+variable "aws_instance_fuJau_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_gegGr_instance_type" {
+  default = "c5.2xlarge"
+}
+
+variable "aws_instance_goJDV_instance_type" {
+  default = "t3.small"
+}
+
+variable "aws_instance_igvSq_instance_type" {
+  default = "t2.small"
+}
+
+variable "aws_instance_sGheK_instance_type" {
+  default = "t2.micro"
+}
+
+variable "aws_instance_tsAsd_instance_type" {
+  default = "t2.medium"
+}
+
