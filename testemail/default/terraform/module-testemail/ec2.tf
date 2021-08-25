feb444be06399e8eb499cf589b99924eb0bd040e
flo test <florian.lambert@cycloid.io>
2021-08-25 13:49:59 +0000

    Update Service Catalog Terraform: testemail

--- testemail/default/terraform/module-testemail/ec2.tf
+++ testemail/default/terraform/module-testemail/ec2.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "BYAbd" {
+resource "aws_instance" "DqoRD" {
   tags = {
     Name         = "cycloid-bastion0-eu-we1-infra"
     client       = "cycloid"
@@ -50,60 +50,7 @@ resource "aws_instance" "BYAbd" {
   vpc_security_group_ids = ["sg-f9db709e", "sg-023dc68f6ce068ff7"]
 }
 
-resource "aws_instance" "RadiN" {
-  tags = {
-    Name                 = "cycloid-monitoring0-eu-we1-infra"
-    client               = "cycloid"
-    env                  = "infra"
-    monitoring_discovery = "false"
-    project              = "infra"
-    role                 = "monitoring"
-  }
-
-  tags_all = {
-    Name                 = "cycloid-monitoring0-eu-we1-infra"
-    client               = "cycloid"
-    env                  = "infra"
-    monitoring_discovery = "false"
-    project              = "infra"
-    role                 = "monitoring"
-  }
-
-  ami                         = "ami-e079f893"
-  associate_public_ip_address = true
-  availability_zone           = "eu-west-1a"
-  cpu_core_count              = 1
-  cpu_threads_per_core        = 1
-  credit_specification {
-    cpu_credits = "standard"
-  }
-
-  disable_api_termination              = true
-  iam_instance_profile                 = "infra_profile"
-  instance_initiated_shutdown_behavior = "stop"
-  instance_type                        = "t2.small"
-  key_name                             = "cycloid"
-  metadata_options {
-    http_endpoint               = "enabled"
-    http_put_response_hop_limit = 1
-    http_tokens                 = "optional"
-  }
-
-  private_ip = "10.42.0.79"
-  root_block_device {
-    delete_on_termination = true
-    iops                  = 100
-    volume_size           = 8
-    volume_type           = "gp2"
-  }
-
-  source_dest_check      = true
-  subnet_id              = "subnet-550fac31"
-  tenancy                = "default"
-  vpc_security_group_ids = ["sg-c0db70a7", "sg-c1db70a6", "sg-023dc68f6ce068ff7"]
-}
-
-resource "aws_instance" "gAHGi" {
+resource "aws_instance" "NpbTS" {
   tags = {
     Name         = "monitoring-prometheus-eu-we1-infra"
     customer     = "cycloid"
@@ -163,5 +110,58 @@ resource "aws_instance" "gAHGi" {
   subnet_id              = "subnet-550fac31"
   tenancy                = "default"
   vpc_security_group_ids = ["sg-0850e3981ee19c3a9", "sg-c1db70a6"]
+}
+
+resource "aws_instance" "cxMYH" {
+  tags = {
+    Name                 = "cycloid-monitoring0-eu-we1-infra"
+    client               = "cycloid"
+    env                  = "infra"
+    monitoring_discovery = "false"
+    project              = "infra"
+    role                 = "monitoring"
+  }
+
+  tags_all = {
+    Name                 = "cycloid-monitoring0-eu-we1-infra"
+    client               = "cycloid"
+    env                  = "infra"
+    monitoring_discovery = "false"
+    project              = "infra"
+    role                 = "monitoring"
+  }
+
+  ami                         = "ami-e079f893"
+  associate_public_ip_address = true
+  availability_zone           = "eu-west-1a"
+  cpu_core_count              = 1
+  cpu_threads_per_core        = 1
+  credit_specification {
+    cpu_credits = "standard"
+  }
+
+  disable_api_termination              = true
+  iam_instance_profile                 = "infra_profile"
+  instance_initiated_shutdown_behavior = "stop"
+  instance_type                        = "t2.small"
+  key_name                             = "cycloid"
+  metadata_options {
+    http_endpoint               = "enabled"
+    http_put_response_hop_limit = 1
+    http_tokens                 = "optional"
+  }
+
+  private_ip = "10.42.0.79"
+  root_block_device {
+    delete_on_termination = true
+    iops                  = 100
+    volume_size           = 8
+    volume_type           = "gp2"
+  }
+
+  source_dest_check      = true
+  subnet_id              = "subnet-550fac31"
+  tenancy                = "default"
+  vpc_security_group_ids = ["sg-c0db70a7", "sg-c1db70a6", "sg-023dc68f6ce068ff7"]
 }
 
