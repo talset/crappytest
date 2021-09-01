b2d95394529af8ea58520dbb29622fcbe1ef32cd
talset <talset55@gmail.com>
2021-09-01 18:39:49 +0200

    clean

--- gaelstack1/default/terraform/module-gaelstack1/tc_module.tf
+++
@@ -1,7 +0,0 @@
-module "gaelstack1" {
-  # aws_instance_jbOxm_instance_type = "t3.small"
-  # aws_instance_nfibD_instance_type = "t3.small"
-  # aws_instance_vgnMd_instance_type = "t3.small"
-  source = "module-gaelstack1"
-}
-
