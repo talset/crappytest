bf09c79f562338961152a89776e66199eea82cb4
flo test <florian.lambert@cycloid.io>
2021-09-08 08:07:12 +0000

    Update Service Catalog Terraform: stackonly5

---
+++ stackonly5/terraform/default/module-stackonly5/variables.tf
@@ -0,0 +1,12 @@
+variable "aws_autoscaling_group_cycloid_demo_workers_workersgroup_7y8gii4fwrr4_desired_capacity" {
+  default = 1
+}
+
+variable "aws_autoscaling_group_cycloidio_website_front_prod_frontsprod_1jpc2znehafpf_desired_capacity" {
+  default = 2
+}
+
+variable "aws_autoscaling_group_external_worker_worker_prod_externalworkersprod_d6v1hqadkm9r_desired_capacity" {
+  default = 4
+}
+
