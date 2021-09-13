2a8ba711e1589c374a902a0e8301c30dfa0bdfef
talset <talset55@gmail.com>
2021-09-13 17:38:06 +0200

    clean

--- aws_security_group/terraform/default/module-aws_security_group/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_security_group_cycloid_demo_workers_prod_instance_sg_description" {
-  default = "Enable SSH access and HTTP from the load balancer only"
-}
-
-variable "aws_security_group_cycloid_demo_workers_prod_metrics_sg_description" {
-  default = "Allow metrics server to collect metrics"
-}
-
--- stackandproject/terraform/default/module-stackandproject/variables.tf
+++
@@ -1,8 +0,0 @@
-variable "aws_cloudfront_distribution_cycloidio_website_cloudfront_prod_enabled" {
-  default = true
-}
-
-variable "aws_cloudfront_distribution_cycloidio_website_cloudfront_staging_enabled" {
-  default = true
-}
-
