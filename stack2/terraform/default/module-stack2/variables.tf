be196af7d62f8d75bcfcb80e71ef376acf884cbe
admin cycloid <disabled@email.cycloid>
2021-09-14 15:35:01 +0000

    Update Service Catalog Terraform: stack2

---
+++ stack2/terraform/default/module-stack2/variables.tf
@@ -0,0 +1,32 @@
+variable "aws_autoscaling_group_cycloid_demo_workers_workersgroup_7y8gii4fwrr4_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_cycloid_lab_workers_workersgroup_ddz9gugzjj8p_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_cycloid_workers_saas_workersgroup_1lw1f7keqyis1_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_cycloidio_website_front_prod_frontsprod_1jpc2znehafpf_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_cycloidio_website_front_staging_frontsstaging_usc7qju0m25n_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_demo_demo_preprod_demo_demo_preprod_lc_version20171027103536517600000001_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_external_worker_worker_prod_externalworkersprod_d6v1hqadkm9r_default_cooldown" {
+  default = 300
+}
+
+variable "aws_autoscaling_group_workers_worker_prod_externalworkersprod_1qkjeewy2z6jw_default_cooldown" {
+  default = 300
+}
+
