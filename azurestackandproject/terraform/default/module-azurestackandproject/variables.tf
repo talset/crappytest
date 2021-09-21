61746abda2df33cd084685abb5d5764dcc467731
admin cycloid <disabled@email.cycloid>
2021-09-21 07:47:59 +0000

    Update Service Catalog Terraform: azurestackandproject

---
+++ azurestackandproject/terraform/default/module-azurestackandproject/variables.tf
@@ -0,0 +1,8 @@
+variable "azurerm_resource_group__subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_terraform_location" {
+  default = "francecentral"
+}
+
+variable "azurerm_resource_group__subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_terraform_name" {
+  default = "cycloid-terraform"
+}
+
