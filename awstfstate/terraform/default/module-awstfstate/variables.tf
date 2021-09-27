593d0d79f2c617d1b5414ad02188d4d1956d5aa8
admin cycloid <disabled@email.cycloid>
2021-09-27 11:54:43 +0000

    Update Service Catalog Terraform: awstfstate

---
+++ awstfstate/terraform/default/module-awstfstate/variables.tf
@@ -0,0 +1,8 @@
+variable "aws_cloudfront_distribution_cycloidio_website_cloudfront_prod_enabled" {
+  default = true
+}
+
+variable "aws_cloudfront_distribution_cycloidio_website_cloudfront_staging_enabled" {
+  default = true
+}
+
