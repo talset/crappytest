d1e63febb320c54a3ec9aedb2a81997f49541006
admin cycloid <disabled@email.cycloid>
2021-09-27 13:04:10 +0000

    Update Service Catalog Terraform: azureextbe

---
+++ azureextbe/terraform/default/module-azureextbe/base.tf
@@ -0,0 +1,9 @@
+resource "azurerm_resource_group" "_subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_terraform" {
+  tags = {
+    "cycloid.io" = "true"
+  }
+
+  location = "francecentral"
+  name     = "cycloid-terraform"
+}
+
