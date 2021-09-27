9e890ce36b8081dbf19aecc04ebe9f5ce4fd7652
admin cycloid <disabled@email.cycloid>
2021-09-27 10:03:23 +0000

    Update Service Catalog Terraform: mitko_test_gcp

---
+++ mitko_test_gcp/terraform/default/module-mitko_test_gcp/variables.tf
@@ -0,0 +1,28 @@
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state__tc_labels_customer" {
+  default = "cycloid-sandbox"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state__tc_labels_cycloidio" {
+  default = "true"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_location" {
+  default = "EU"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_name" {
+  default = "cycloid-sandbox-terraform-remote-state"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_project" {
+  default = "cycloid-sandbox"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_storage_class" {
+  default = "STANDARD"
+}
+
+variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_tc_category" {
+  default = "cloud_storage"
+}
+
