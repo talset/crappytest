2f7c65cde2dfd64f46e9aced198a6758b2929b2f
admin cycloid <disabled@email.cycloid>
2021-09-28 10:19:48 +0000

    Delete Service Catalog: azureextbe

--- azureextbe/terraform/default/module-azureextbe/base.tf
+++
@@ -1,9 +0,0 @@
-resource "azurerm_resource_group" "_subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_terraform" {
-  tags = {
-    "cycloid.io" = "true"
-  }
-
-  location = "francecentral"
-  name     = "cycloid-terraform"
-}
-
