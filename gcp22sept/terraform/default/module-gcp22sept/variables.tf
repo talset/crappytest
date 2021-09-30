a7f1470758b67b43f5c60ec85c7f17baaaa4b3dc
talset <talset55@gmail.com>
2021-09-30 09:36:10 +0200

    clean

--- gcp22sept/terraform/default/module-gcp22sept/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "google_storage_bucket_cycloid_demo_terraform_remote_state_name" {
-  default = "cycloid-demo-terraform-remote-state"
-}
-
--- gcpbuck/terraform/default/module-gcpbuck/variables.tf
+++
@@ -1,4 +0,0 @@
-variable "google_storage_bucket_cycloid_sandbox_terraform_remote_state_name" {
-  default = "cycloid-sandbox-terraform-remote-state"
-}
-
