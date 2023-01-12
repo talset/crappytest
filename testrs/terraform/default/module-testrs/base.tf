383da8038d732765aa6c019b002187f153b17eaf
talset <talset55@gmail.com>
2023-01-12 18:05:22 +0100

    clean

--- testrs/terraform/default/module-testrs/base.tf
+++
@@ -1,5 +0,0 @@
-resource "azurerm_resource_group" "_subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_demo" {
-  location = "francecentral"
-  name     = var.azurerm_resource_group__subscriptions_edce4685_4988_4a09_8b4a_3d5b917022cd_resourcegroups_cycloid_demo_name
-}
-
