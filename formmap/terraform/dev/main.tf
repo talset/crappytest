a8505786c5cebfc1252c70721355f7722a2bf1da
Florian Lambert <florian.lambert@cycloid.io>
2024-02-12 15:17:23 +0000

    Create Config Repository by crappy

--- formmap/terraform/dev/main.tf
+++ formmap/terraform/dev/main.tf
@@ -11,12 +11,6 @@ module "myapp" {
   source = "./module-sample"
 
   cronjobs = {
-    quiz = {
-      cpu        = 256
-      entrypoint = "/bin/php"
-      memory     = 1024
-      schedule   = "*/30 * * * ? *"
-    }
   }
   env          = var.env
   organization = var.organization
